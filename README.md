23c7c24be7c099269bf06525ff506f2cf7516dec
Sam Corcoran <samcorcoran@improbable.io>
2016-12-16 16:17:12 +0000

    version 9.0.2

---
+++ README.md
@@ -0,0 +1,30 @@
+# BlankProject
+---
+
+*Copyright Improbable 2016*
+
+- *Accompanying Tutorial*: [spatialos.improbable.io/docs/latest/tutorials/your-first-project/lesson1](https://spatialos.improbable.io/docs/latest/tutorials/your-first-project/lesson1)
+
+- *GitHub Repository*: [github.com/spatialos/BlankProject](https://github.com/spatialos/BlankProject)
+
+---
+
+## Introduction
+
+This is a blank SpatialOS project that you can use to build your own SpatialOS application.
+
+It contains:
+
+* A SpatialOS project descriptor.
+* Unity worker
+* Bare-bones Launch Configurations for automatically and manually launching workers.
+* Bare-bones Client and Physics Scenes with a `Bootstrap.cs` file.
+* An example deployment configuration, `deploy.json`.
+* `.gitignore` files for a SpatialOS project.
+
+## Using the BlankProject
+
+For instructions on how to use and set-up this BlankProject, follow the Your First Project tutorial, available in
+the [Improbable documentation](https://spatialos.improbable.io/docs/).
+
+To report bugs or to give feedback on this project, please contact SpatialOS Support.
---
+++ workers/unity/Assets/Bootstrap.cs
@@ -0,0 +1,38 @@
+﻿using Improbable.Unity;
+using Improbable.Unity.Configuration;
+using Improbable.Unity.Core;
+using UnityEngine;
+
+// Placed on a gameobject in client scene to execute connection logic on client startup
+public class Bootstrap : MonoBehaviour
+{
+    public WorkerConfigurationData Configuration = new WorkerConfigurationData();
+
+    public void Start()
+    {
+        SpatialOS.ApplyConfiguration(Configuration);
+
+        switch (SpatialOS.Configuration.EnginePlatform)
+        {
+            case EnginePlatform.FSim:
+                SpatialOS.OnDisconnected += reason => Application.Quit();
+
+                var targetFramerate = 120;
+                var fixedFramerate = 20;
+
+                Application.targetFrameRate = targetFramerate;
+                Time.fixedDeltaTime = 1.0f / fixedFramerate;
+                break;
+            case EnginePlatform.Client:
+                SpatialOS.OnConnected += OnConnected;
+                break;
+        }
+
+        SpatialOS.Connect(gameObject);
+    }
+
+    public void OnConnected()
+    {
+        Debug.Log("Bootstrap connected to SpatialOS...");
+    }
+}
