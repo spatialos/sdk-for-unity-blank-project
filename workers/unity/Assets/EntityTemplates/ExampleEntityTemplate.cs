f117384323806b3c4a0dc0566784e29269f4cade
Sam Corcoran <samcorcoran@improbable.io>
2016-12-16 17:34:09 +0000

    Added an ExampleEntity entity baked into a snapshot
    Includes entity template, prefab, snapshot containing entity and customized Unity Editor menu to provide an easy way of programmatically determining the contents of a snapshot

---
+++ workers/unity/Assets/EntityTemplates/ExampleEntityTemplate.cs
@@ -0,0 +1,27 @@
+﻿using Improbable.General;
+using Improbable.Worker;
+using Improbable.Math;
+using Improbable.Unity.Core.Acls;
+using UnityEngine;
+
+namespace Assets.EntityTemplates
+{
+    public class ExampleEntityTemplate : MonoBehaviour
+    {
+        // Template definition for a Example entity
+        public static SnapshotEntity GenerateExampleSnapshotEntityTemplate()
+        {
+            // Set name of Unity prefab associated with this entity
+            var exampleEntity = new SnapshotEntity { Prefab = "ExampleEntity" };
+
+            // Define components attached to snapshot entity
+            exampleEntity.Add(new WorldTransform.Data(new WorldTransformData(new Coordinates(-5, 10, 0))));
+
+            // Grant FSim (server-side) workers write-access over all of this entity's components, read-access for visual (e.g. client) workers
+            var acl = Acl.GenerateServerAuthoritativeAcl(exampleEntity);
+            exampleEntity.SetAcl(acl);
+
+            return exampleEntity;
+        }
+    }
+}
